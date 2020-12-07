1722fc8268350a33890aff0987e028354afe2b4d
Lizaveta Kemerava <kemerava@mail.yu.edu>
2020-12-06 22:05:58 -0500

    added the Item model

--- hello/models.py
+++ hello/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class Greeting(models.Model):
     when = models.DateTimeField("date created", auto_now_add=True)
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=500)
+    rating = models.IntegerField()
