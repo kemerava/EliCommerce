03fdbd23eeec170565a4a4004dba360a6c0511bf
Lizaveta Kemerava <kemerava@mail.yu.edu>
2020-12-19 13:44:12 -0500

    moved to postgres

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -14,7 +14,6 @@ import os
 
 import django_heroku
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -78,10 +77,16 @@ WSGI_APPLICATION = "gettingstarted.wsgi.application"
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
-    }
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'elicommercedb',
+        'USER': 'elizabeth',
+        'PASSWORD': 'family7',
+        'HOST': 'localhost',
+        'PORT': '5432'
+}
+
 }
 
 # Password validation
